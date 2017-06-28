b001a8d8a821bbd2e24c5df584f3dcc51b235b7e
Jesse <jesse_mclellan@yahoo.com>
2017-06-27 18:25:54 -0700

    :)

--- courses_assignment/settings.py
+++ courses_assignment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gtus7z!9=03&7s1=ca6i=s5=4=_io#b*wolc6x-ar-($th^tyx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.220.18.116']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
